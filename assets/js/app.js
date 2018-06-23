c590aef5be52e423f478f2dfb317a845a5d0fff1
NickDevoctomy <nick@devoctomy.com>
2018-06-23 07:20:11 +0100

    Created function for switching tabs via a script, and home page query argument.  Also fixed spacing issue with social buttons and link on new ideas blog.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 var Tabs = (function() {
   var s;
+  var currentIdx = 0;
+  var prevIdx = 0;  
 
   return {
     settings: {
       tabs: document.getElementsByClassName('tabs__item'),
-      tab: document.getElementsByClassName('tab')
+      tabPages: document.getElementsByClassName('tab')
     },
 
     init: function() {
@@ -12,34 +14,42 @@ var Tabs = (function() {
       this.display();
       this.click();
     },
+	
+	selectExternal: function(i) {
+	  prevIdx = currentIdx;
+	  currentIdx = i;
+      s.tabPages[prevIdx].style.display = 'none';
+      s.tabPages[prevIdx].classList.remove('active');
+      s.tabs[prevIdx].classList.remove('active');
+      s.tabPages[currentIdx].style.display = 'block';
+      s.tabPages[currentIdx].classList.add('active');
+      s.tabs[currentIdx].classList.add('active');
+	},
 
     display: function() {
-      if (s.tab.length) {
-        [].forEach.call(s.tab, function(tab) {
-          tab.style.display = 'none';
+      if (s.tabPages.length) {
+        [].forEach.call(s.tabPages, function(tabPage) {
+          tabPage.style.display = 'none';
         });
-        s.tab[0].style.display = 'block';
-        s.tab[0].classList.add('active');
+        s.tabPages[0].style.display = 'block';
+        s.tabPages[0].classList.add('active');
         s.tabs[0].classList.add('active');
       }
     },
 
     click: function() {
       if (s.tabs.length) {
-        var currentIdx = 0,
-            prevIdx = currentIdx;
-
         [].forEach.call(s.tabs, function(tab, idx) {
           tab.addEventListener('click', function() {
             prevIdx = currentIdx;
             currentIdx = idx;
 
             if (prevIdx !== currentIdx) {
-              s.tab[prevIdx].style.display = 'none';
-              s.tab[prevIdx].classList.remove('active');
+              s.tabPages[prevIdx].style.display = 'none';
+              s.tabPages[prevIdx].classList.remove('active');
               s.tabs[prevIdx].classList.remove('active');
-              s.tab[currentIdx].style.display = 'block';
-              s.tab[currentIdx].classList.add('active');
+              s.tabPages[currentIdx].style.display = 'block';
+              s.tabPages[currentIdx].classList.add('active');
               s.tabs[currentIdx].classList.add('active');
             }
           });
@@ -99,8 +109,22 @@ var wow = new WOW({
   animateClass: 'fade-in'
 });
 
+function getParameterByName(name, url)
+{
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+  results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   Tabs.init();
   Preview.init();
-  wow.init();
+  wow.init(); 
+  
+  var tab = getParameterByName('tab');
+  if(tab) Tabs.selectExternal(tab);
 });
